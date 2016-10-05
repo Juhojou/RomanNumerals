34dca477692149ed869ffa30a32b9dcf828fb678
jkananen <jkananen@10.30.4.22>
2016-10-05 15:11:55 +0300

    progress

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,25 @@
 
 public class RomanNumerals {
+	
+	private int convertedNumber=0;
+	
 	public int convertToInteger(String romanNum) {
-		// To be Implemented
-		return 0;
-		
+		String [] parts = romanNum.split("");
+		for (int i=0; i<parts.length; i++){
+				if (parts[i].equals("I"))
+					convertedNumber = convertedNumber + 1;
+				if (parts[i].equals("V")){
+					convertedNumber = 5;
+					if (parts.length > 1 && parts[i-1].equals("I"))
+						convertedNumber = convertedNumber - 1;
+					else if (parts.length > 1 && parts[i-1].equals("I") == false)
+						convertedNumber = convertedNumber + 1;
+				}
+
+				if (parts[i].equals("X"))
+					convertedNumber = convertedNumber + 10;
+		}
+		return convertedNumber;
 	}
+	
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -3,10 +3,51 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestRomanNumerals {
+	
+	RomanNumerals conventer = new RomanNumerals();
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_MakeNewConventer() {
+		assertNotNull(null, conventer);
+	}
+	
+	@Test
+	public void test_convert_I_to_1() {
+		checkEquals(1, "I");
+	}
+	
+	@Test
+	public void test_convert_II_to_2() {
+		checkEquals(2, "II");
+	}
+	
+	@Test
+	public void test_convert_III_to_3() {
+		checkEquals(3, "III");
+	}
+	
+	@Test
+	public void test_convert_V_to_5() {
+		checkEquals(5, "V");
+	}
+	
+	@Test
+	public void test_convert_X_to_10() {
+		checkEquals(10, "X");
+	}
+	
+	@Test
+	public void test_convert_IV_to_4() {
+		checkEquals(4, "IV");
+	}
+	
+	@Test
+	public void test_convert_VI_to_6() {
+		checkEquals(6, "VI");
+	}
+
+	private void checkEquals(int number, String romanNumber) {
+		assertEquals(number, conventer.convertToInteger(romanNumber));
 	}
 
 }
